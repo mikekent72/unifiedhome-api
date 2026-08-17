83f56a0c994974a56f8178d8ff6f0dcbee6fa987
mikekent72 <mikekent727@gmail.com>
2026-08-17 15:50:03 +0100

    Add room entity and repository

---
+++ src/main/java/com/unifiedhome/api/model/Room.java
@@ -0,0 +1,38 @@
+package com.unifiedhome.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+    private String name;
+    
+    public Room() {
+    }
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/unifiedhome/api/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.unifiedhome.api.repository;
+
+import com.unifiedhome.api.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    
+}
