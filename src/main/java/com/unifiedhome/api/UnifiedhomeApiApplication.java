5eaec462d35436d5eddd58989d2695a4f9f11bfd
mikekent72 <mikekent727@gmail.com>
2026-08-17 13:31:39 +0100

    Initialise UnifiedHome API project

---
+++ src/main/java/com/unifiedhome/api/UnifiedhomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.unifiedhome.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedhomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedhomeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifiedhome/api/UnifiedhomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifiedhome.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedhomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
